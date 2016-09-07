a0417653e7f0eaa90beaeb483fb0897d701a77ec
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-09-07 16:30:56 +0600

    UploadedFileDAO impl

---
+++ model-impl/src/main/java/kz/theeurasia/documentor/beans/dao/DefaultUploadedFileDAO.java
@@ -0,0 +1,64 @@
+package kz.theeurasia.documentor.beans.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import kz.theeurasia.documentor.api.dao.DAOException;
+import kz.theeurasia.documentor.api.dao.SearchParameters;
+import kz.theeurasia.documentor.api.dao.UploadedFileDAO;
+import kz.theeurasia.documentor.model.DocumentPackage_;
+import kz.theeurasia.documentor.model.UploadedFile;
+import kz.theeurasia.documentor.model.UploadedFile_;
+
+@Stateless
+public class DefaultUploadedFileDAO extends DefaultGenericDAO<Integer, UploadedFile>
+	implements UploadedFileDAO {
+
+    protected DefaultUploadedFileDAO() {
+	super(UploadedFile.class);
+    }
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
+    public List<UploadedFile> findByIdNumber(SearchParameters parameters) throws DAOException {
+	try {
+	    CriteriaBuilder cb = em.getCriteriaBuilder();
+	    CriteriaQuery<UploadedFile> cq = cb.createQuery(entityClass);
+	    Root<UploadedFile> root = cq.from(entityClass);
+
+	    List<Predicate> whereOptions = new ArrayList<>();
+
+	    // request status
+	    if (parameters.getIdNumberPattern() != null && !parameters.getIdNumberPattern().trim().isEmpty()
+		    && parameters.getIdNumberPattern().trim().length() >= 3) {
+		String pattern = "%" + parameters.getIdNumberPattern().trim() + "%";
+		whereOptions
+			.add(cb.like(root.get(UploadedFile_.documentPackage).get(DocumentPackage_.idNumber), pattern));
+	    }
+
+	    if (whereOptions.isEmpty())
+		return null;
+
+	    cq.select(root)
+		    .where(cb.and(whereOptions.toArray(new Predicate[0])))
+		    .orderBy(cb.desc(root.get(UploadedFile_.documentPackage).get(DocumentPackage_.created)),
+			    cb.asc(root.get(UploadedFile_.name)));
+
+	    TypedQuery<UploadedFile> q = em.createQuery(cq);
+	    return resultList(q);
+
+	} catch (Throwable e) {
+	    throw new DAOException(e);
+	}
+    }
+
+}
